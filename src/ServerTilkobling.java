9f8a9db82a623d08c5085fa3bdfe2e2df201280e
Hauken1 <henrik.hauk.92@gmail.com>
2016-03-17 14:08:51 +0100

    Fungerende kommunikasjon i mellom server og hdl

--- src/ServerTilkobling.java
+++ src/ServerTilkobling.java
@@ -6,6 +6,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
@@ -41,6 +42,7 @@ public class ServerTilkobling extends JFrame {
 	private ArrayBlockingQueue<String> messages = new ArrayBlockingQueue<String>(50);
 		
 	private String IP = "192.168.10.141";
+	private String HDLip = "192.168.10.255";
 	
 	public ServerTilkobling() {
 		
@@ -167,10 +169,9 @@ public class ServerTilkobling extends JFrame {
 	
 	private void sendPacketToHDL () {
 		displayMessage("\n\nEcho data to HDL....");
-		//InetAddress ip = InetAddress.getByName("192.168.10.141");
-		//byte[] bytes = ip.getAddress();
-		byte[] HDLData = new byte[30];
-		  ByteBuffer buf = ByteBuffer.allocate(100);
+		
+		byte[] HDLData = new byte[31];
+		//ByteBuffer buf = ByteBuffer.allocate(100);
 		  
 		String ip1 = "192";
 		String ip2 = "168";
@@ -181,33 +182,6 @@ public class ServerTilkobling extends JFrame {
 		int ipInt3 = Integer.parseInt(ip3);
 		int ipInt4 = Integer.parseInt(ip4);
 		
-		String h = "H";
-		String d = "D";
-		String l1 = "L";
-		String m = "M";
-		String i = "I";
-		String r = "R";
-		String a = "A";
-		String c = "C";
-		String l2 = "L";
-		String e = "E";
-
-		/*
-		int text1 = Integer.parseInt(h);
-		int text2 = Integer.parseInt(d);
-		int text3 = Integer.parseInt(l1);
-		int text4 = Integer.parseInt(m);
-		int text5 = Integer.parseInt(i);
-		int text6 = Integer.parseInt(r);
-		int text7 = Integer.parseInt(a);
-		int text8 = Integer.parseInt(c);
-		int text9 = Integer.parseInt(l2);
-		int text10 = Integer.parseInt(e);
-		*/
-		String s = "HDLMIRACLE";
-		//HDLData[5] = s.(StandardCharsets.US_ASCII);i
-		
-		//char[] mir = {'H', 'D', 'L','M','I','R','A','C','L','E'};
 		String mir = "HDLMIRACLE";
 		int[] mirInt = new int[mir.length()];
 		
@@ -216,6 +190,7 @@ public class ServerTilkobling extends JFrame {
 				mirInt[k] = mir.charAt(k);
 			} catch(NumberFormatException numberE){};
 		}
+		/*
 		HDLData[0] = (byte) ipInt1;
 		HDLData[1] = (byte) ipInt2;
 		HDLData[2] = (byte) ipInt3;
@@ -248,59 +223,68 @@ public class ServerTilkobling extends JFrame {
 		HDLData[28] = (byte) 1;		
 		HDLData[29] = (byte) 208;
 		HDLData[30] = (byte) 164;
-
 		/*
-		int n = (int) Integer.parseInt(s);
-		for (int j = 0; 0 <= s.length(); j++  ){
-			HDLData[4+j] = (byte) s.indexOf(j); 
-		}
-		*/
+		 * 
+		 */
+		HDLData[0] = (byte) ipInt1;
+		HDLData[1] = (byte) ipInt2;
+		HDLData[2] = (byte) ipInt3;
+		HDLData[3] = (byte) ipInt4;
+		
+		HDLData[4] = (byte) mirInt[0];
+		HDLData[5] = (byte) mirInt[1];
+		HDLData[6] = (byte) mirInt[2];
+		HDLData[7] = (byte) mirInt[3];
+		HDLData[8] = (byte) mirInt[4];
+		HDLData[9] = (byte) mirInt[5];
+		HDLData[10] = (byte) mirInt[6];
+		HDLData[11] = (byte) mirInt[7];
+		HDLData[12] = (byte) mirInt[8];
+		HDLData[13] = (byte) mirInt[9];
+		HDLData[14] = (byte) 170;
+		HDLData[15] = (byte) 170;
+		HDLData[16] = (byte) 15;		
+		HDLData[17] = (byte) 12;	
+		HDLData[18] = (byte) 254;
+		HDLData[19] = (byte) 255;
+		HDLData[20] = (byte) 254;	
+		HDLData[21] = (byte) 0;
+		HDLData[22] = (byte) 49;
+		HDLData[23] = (byte) 1;
+		HDLData[24] = (byte) 17;		
+		HDLData[25] = (byte) 1;	
+		//Lys på
+		HDLData[26] = (byte) 100;
+		//Lys av
+		//HDLData[26] = (byte) 0;
+		
+		HDLData[27] = (byte) 0;
+		HDLData[28] = (byte) 1;		
+		//Lys på
+		HDLData[29] = (byte) 151;
+		HDLData[30] = (byte) 15;
+		//Lys av
+		//HDLData[29] = (byte) 208;
+		//HDLData[30] = (byte) 164;
+		
+		/*
 		for (byte b : HDLData) {
 		    System.out.println(b & 0xFF);
 		}
-		
+		*/
 				
-		/*
-		
-		    // Get the buffer's capacity
-		  int capacity = buf.capacity(); // 10
-
-		    // Use the absolute put().
-		    // This method does not affect the position.
-		   buf.put((byte)192); // position=0
-		   int rem = buf.remaining();
-		   System.out.println(rem);
-		//ByteBuffer buf = ByteBuffer.wrap(HDLData);
-		   buf.put((byte)168);
-		   rem = buf.remaining();
-		   System.out.println(rem);
-		   buf.put((byte)10);
-		   buf.put((byte)141);
-		   rem = buf.remaining();
-		   System.out.println(rem);
-		   String test = new String(buf.array(), Charset.forName("UTF-8"));
-		   System.out.println(test);
-		   
-		  */
-		  
-		   
-		   
-		   /*
-		String addressPart1 = "192";
-		String addressPart2 = "168";
-		InetAddress addressPart3 = InetAddress.getByName("10");
-		InetAddress addressPart4 = InetAddress.getByName("141");
-		
-		HDLData = ip.getAddress();
-		HDLData = 
-		HDLData  
-		*/ 
-		
-		
-		
-		
-		//DatagramPacket HDLPacket = new DatagramPacket(buf, length, address, port);
-		
+		try {
+			DatagramPacket sendPacket;
+			sendPacket = new DatagramPacket(HDLData,
+			        HDLData.length, InetAddress.getByName(HDLip), 6000);
+			socket.send(sendPacket);
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 	
 }
